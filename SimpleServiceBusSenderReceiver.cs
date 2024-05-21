cdb459b0504d02e27b46b4ccf2b93af392ee2698
Richard Siegfried Heintze <v-richardsi@microsoft.com>
2024-05-21 05:03:30 -0700

    Recreate repo with the .csproj and .cs in the top level.

---
+++ SimpleServiceBusSenderReceiver.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace SimplerServiceBusSendReceiveDemo;
+
+public class SimpleServiceBusSenderReceiver
+{
+    private readonly ILogger<SimpleServiceBusSenderReceiver> _logger;
+
+    public SimpleServiceBusSenderReceiver(ILogger<SimpleServiceBusSenderReceiver> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(SimpleServiceBusSenderReceiver))]
+    public async Task Run(
+        [ServiceBusTrigger("mainqueue001", Connection = "serviceBusConnectionString")]
+        ServiceBusReceivedMessage message,
+        ServiceBusMessageActions messageActions)
+    {
+        _logger.LogInformation("Built at Mon May 20 21:33:41 2024 Message ID: {id} Body: {body} Content-type: {contentType}", message.MessageId, message.Body, message.ContentType);
+
+        // Complete the message
+        await messageActions.CompleteMessageAsync(message);
+    }
+}
+
