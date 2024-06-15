622f74922efc92ef7332b9468569a636555cb4fe
siegfried01 <sheintze@hotmail.com>
2024-06-15 07:18:04 -0700

    Add Powershell to update the date timestamp and version in SimpleServiceBusSenderReceiver.cs

--- SimpleServiceBusSendReceiveAzureFuncs/SimpleServiceBusSenderReceiver.cs
+++ SimpleServiceBusSendReceiveAzureFuncs/SimpleServiceBusSenderReceiver.cs
@@ -17,7 +17,21 @@ namespace SimpleServiceBusSendReceiveAzureFuncs
         [Function("SimpleServiceBusReceiver")]
         public void Run([ServiceBusTrigger("mainqueue001", Connection = "ServiceBusConnection")] ServiceBusReceivedMessage message)
         {
-            _logger.LogInformation("Built at Wed Jun  5 12:54:20 2024 Message ID: {id} Body: {body} Content-type: {contentType}", message.MessageId, message.Body, message.ContentType);
+            _logger.LogInformation("Built at Fri Jun 14 18:24:36 2024 Version 000000002 Message ID: {id} Body: {body} Content-type: {contentType}", message.MessageId, message.Body, message.ContentType);
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
