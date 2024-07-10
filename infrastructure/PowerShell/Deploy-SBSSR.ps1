794d8b8f2e3835dfd65d23530b5139c68821ee75
Paxton Perry <v-paperry@microsoft.com>
2024-07-09 17:03:59 -0700

    Made further updates to the Deploy-SBSSR script - I confirmed that it runs to completion

--- infrastructure/PowerShell/Deploy-SBSSR.ps1
+++ infrastructure/PowerShell/Deploy-SBSSR.ps1
@@ -1,7 +1,12 @@
-﻿If ($env:USERNAME -eq "shein") { $env:name='SBusSndRcv' } 
+﻿# If it hasn't been done already
+# Install-Module -Name Az -AllowClobber -Scope CurrentUser
+# Connect-AzAccount
+
+
+If ($env:USERNAME -eq "shein") { $env:name='SBusSndRcv' } `
     Else { $env:name="SBSSR-$($env:USERNAME)" }
 $env:rg="rg-$($env:name)"
-$env:loc=If ($env:AZ_DEFAULT_LOC) { $env:AZ_DEFAULT_LOC} 
+$env:loc=If ($env:AZ_DEFAULT_LOC) { $env:AZ_DEFAULT_LOC} `
     Else {'eastus2'}
 $env:uniquePrefix="$(If ($env:USERNAME -eq "v-richardsi") {"xizdf"} 
     ElseIf ($env:USERNAME -eq "v-paperry") { "iucpl" } 
@@ -35,9 +40,62 @@ $env:funcPlanName="$($env:uniquePrefix)-plan-func"
 $env:serviceBusNS="$($env:uniquePrefix)-servicebus"
 
 # There is no Resource Group called DefaultResourceGroup-WUS2 - Where is this coming from?
+# 13c9725f-d20a-4c99-8ef4-d7bb78f98cff is CST - E Demos and POCs
 $env:logAnalyticsWS= If ($env:USERNAME -eq "shein") { 
     "/subscriptions/acc26051-92a5-4ed1-a226-64a187bc27db/resourceGroups/DefaultResourceGroup-WUS2/providers/Microsoft.OperationalInsights/workspaces/DefaultWorkspace-acc26051-92a5-4ed1-a226-64a187bc27db-WUS2" } 
     else {   "/subscriptions/13c9725f-d20a-4c99-8ef4-d7bb78f98cff/resourceGroups/defaultresourcegroup-wus2/providers/microsoft.operationalinsights/workspaces/defaultworkspace-13c9725f-d20a-4c99-8ef4-d7bb78f98cff-wus2" }
 
+# Create an Owner tag for resources
+$tags = @{"Owner"="Sbox360"}
+
 $StartTime = $(get-date)
 
+# Check to see if the Resource Group exists or not
+$resourceGroupExists = Get-AzResourceGroup -Name $env:rg -ErrorAction SilentlyContinue
+
+if ($resourceGroupExists) {
+    # Continue
+}
+else {
+    az group create --name $env:rg --location $env:loc
+}
+
+# Set the Resource Group tags
+Set-AzResourceGroup -Name $env:rg -Tag $tags
+
+# This a Phase 1 deployment - It will deploy a Function App, WebApp (for verification only), Storage Accounts and no VNet and no PEP
+az deployment group create --name $env:name --resource-group $env:rg --mode Incremental   `
+     --template-file  "deploy-ServiceBusSimpleSendReceive.bicep"                             `
+     --parameters                                                                            `
+     "{'uniquePrefix'                   : {'value': '$env:uniquePrefix'}}"                   `
+     "{'location'                       : {'value': '$env:loc'}}"                            `
+     "{'myIPAddress'                    : {'value': '$env:myIPAddress'}}"                    `
+     "{'noManagedIdent'                 : {'value': $noManagedIdent}}"                       `
+     "{'functionAppSku'                 : {'value': '$env:functionAppSku'}}"                 `
+     "{'serviceBusSku'                  : {'value': '$env:serviceBusSku'}}"                  `
+     "{'usePremiumServiceBusFunctionApp': {'value': $usePremiumServiceBusFunctionApp}}"      `
+     "{'useServiceBusFireWall'          : {'value': $useServiceBusFireWall}}"                `
+     "{'createVNetForPEP'               : {'value': $createVNetForPEP}}"                     `
+     "{'createWebAppTestPEP'            : {'value': $createWebAppTestPEP}}"                  `
+     "{'useKVForStgConnectionString'    : {'value': $useKVForStgConnectionString}}"          `
+     "{'useSourceControlLoadTestCode'   : {'value': $useSourceControlLoadTestCode}}"         `
+     "{'storageAccountName'             : {'value': '$env:storageAccountName'}}"             `
+     "{'functionAppName'                : {'value': '$env:functionAppName'}}"                `
+     "{'functionPlanName'               : {'value': '$env:funcPlanName'}}"                   `
+     "{'serviceBusNS'                   : {'value': '$env:serviceBusNS'}}"                   `
+     "{'serviceBusQueueName'            : {'value': '$env:serviceBusQueueName'}}"            `
+     "{'logAnalyticsWS'                 : {'value': '$env:logAnalyticsWS'}}"                 `
+| ForEach-Object { $_ -replace "`r", ""}
+   write-output "end deploy $(Get-Date)"
+
+# Here are the resulting resources:
+# aztblogsv12g5jjoo3nvulus
+# iucpl-appins
+# iucpl-detector
+# iucpl-failure anomalies
+# iucpl-func - This one takes longer as it is a Function App
+# iucpl-plan-func
+# iucpl-plan-web
+# iucpl-servicebus
+# iucpl-webapp
+# iucplfuncstg
